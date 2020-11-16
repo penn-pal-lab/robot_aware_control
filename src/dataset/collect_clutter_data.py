d0a4647e1037c56bd4be031d79b2eea0f0fb1f1d
ed <husedward@gmail.com>
2020-11-16 16:26:57 -0500

    svg mr inpaint data collection

--- src/dataset/collect_clutter_data.py
+++ src/dataset/collect_clutter_data.py
@@ -165,9 +165,7 @@ def collect_demo_cem_data():
     ep_len = 12  # gonna be off by -1 because of reset but whatever
 
     config, _ = argparser()
-    config.norobot_pixels_ob = (
-        True  # whether to inpaint the robot pixels in the observation
-    )
+    config.norobot_pixels_ob = True  # whether to inpaint the robot in observation
 
     config.reward_type = "inpaint"
     config.demo_dir = "demos/straight_push"
@@ -184,9 +182,9 @@ def collect_svg_data():
     Collect 7k noisy pushing, 3k truly random demonstrations
     Each demo is around 7-14 steps long, and the dataset will be around 100k images total
     """
-    num_workers = 1
+    num_workers = 10
     num_push = 7000 // num_workers
-    num_rand = 100 // num_workers
+    num_rand = 3000 // num_workers
     record = True
     ep_len = 12  # gonna be off by 1 because of reset but whatever
 
@@ -200,7 +198,7 @@ def collect_svg_data():
     config.camera_ids = [0, 1]
     config.temporal_beta = 0.3  # control random policy's temporal correlation
     config.action_noise = 0.5
-    # create_demo_dataset(config, num_push, num_workers, record, "straight_push", ep_len)
+    create_demo_dataset(config, num_push, num_workers, record, "straight_push", ep_len)
     create_demo_dataset(
         config, num_rand, num_workers, record, "temporal_random_robot", ep_len
     )
