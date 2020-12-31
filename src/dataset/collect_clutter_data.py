e672c3aab535fff4016b1e7d34c55b7719f33d5f
Edward Hu <13341926+EdwardHuCS@users.noreply.github.com>
2020-12-30 16:15:11 -0800

    svg data with realistic robot mask

--- src/dataset/collect_clutter_data.py
+++ src/dataset/collect_clutter_data.py
@@ -201,7 +201,7 @@ def collect_svg_data():
     Collect 7k noisy pushing, 3k truly random demonstrations
     Each demo is around 7-14 steps long, and the dataset will be around 100k images total
     """
-    num_workers = 1
+    num_workers = 10
     num_push = 20000 // num_workers
     num_rand = 10000 // num_workers
     record = False
@@ -211,7 +211,8 @@ def collect_svg_data():
     config.norobot_pixels_ob = True
     config.inpaint_eef = True
     config.reward_type = "inpaint"
-    config.demo_dir = "demos/realvp_random_mask_svg_train"
+    config.robot_mask_with_obj = False
+    config.demo_dir = "demos/realvp_random_realisticmask_svg_train"
     config.most_recent_background = False
     config.multiview = True
     config.img_dim = 64
