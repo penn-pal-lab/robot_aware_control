1c5bf717ca04d0f4d589cd1b3432bc677c7e3636
ed <husedward@gmail.com>
2021-03-01 16:39:30 -0500

    baxter joint pos dataloader

---
+++ src/dataset/baxter_joint_pos_dataloaders.py
@@ -0,0 +1,69 @@
+import os
+import random
+
+import ipdb
+import numpy as np
+import torch
+import torchvision.transforms as tf
+from sklearn.model_selection import train_test_split
+from src.dataset.joint_pos_dataset import JointPosDataset
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import random_split
+from torch.utils.data.sampler import WeightedRandomSampler
+from torchvision.datasets.folder import has_file_allowed_extension
+import pickle
+
+def create_finetune_loaders(config):
+    # finetune on baxter left data
+    file_type = "hdf5"
+    files = []
+    file_labels = []
+    # motion info
+    with open(config.world_error_dict, "rb") as f:
+        motion_info = pickle.load(f)
+    data_path = os.path.join(config.data_root, "new_hdf5")
+    for d in os.scandir(data_path):
+        if d.is_file() and has_file_allowed_extension(d.path, file_type):
+            if f"baxter_left" in d.path:
+                high_error = any([x["high_error"] for x in motion_info[d.path]])
+                if high_error:
+                    files.append(d.path)
+                    file_labels.append("baxter")
+
+    files = sorted(files)
+    random.seed(config.seed)
+    random.shuffle(files)
+
+    n_test = config.finetune_num_test
+    n_train = config.finetune_num_train
+
+    X_test = files[:n_test]
+    y_test = file_labels[:n_test]
+
+    X_train = files[n_test: n_test + n_train]
+    y_train = file_labels[n_test: n_test + n_train]
+    print("loaded finetuning data", len(X_train) + len(X_test))
+
+    augment_img = config.img_augmentation
+    train_data = JointPosDataset(X_train, y_train, config, augment_img=augment_img)
+    test_data = JointPosDataset(X_test, y_test, config)
+
+    train_loader = DataLoader(
+        train_data,
+        num_workers=config.data_threads,
+        batch_size=config.batch_size,
+        shuffle=True,
+        drop_last=False,
+        pin_memory=True,
+        generator=torch.Generator().manual_seed(config.seed),
+    )
+    test_loader = DataLoader(
+        test_data,
+        num_workers=config.data_threads,
+        batch_size=config.test_batch_size,
+        shuffle=True,
+        drop_last=False,
+        pin_memory=True,
+        generator=torch.Generator().manual_seed(config.seed),
+    )
+    return train_loader, test_loader
