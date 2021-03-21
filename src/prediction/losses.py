fixed num world pixels computation for loss

src/prediction/losses.py
@@ -21,9 +21,9 @@ def dontcare_mse_criterion(prediction, target, mask, robot_weight):
     mask = mask.type(torch.bool)
     repeat_mask = mask.repeat(1,3,1,1) # repeat channel dim
     diff[repeat_mask] *= robot_weight
-    num_world_pixels = (~repeat_mask).sum()
-    mean_squared_err = torch.sum(diff ** 2) / num_world_pixels
-    return mean_squared_err
+    num_world_pixels = (~repeat_mask).sum((1,2,3))
+    mean_err = torch.mean((diff ** 2).sum((1,2,3)) / num_world_pixels)
+    return mean_err
 
 def dontcare_l1_criterion(prediction, target, mask, robot_weight):
     """
@@ -35,9 +35,9 @@ def dontcare_l1_criterion(prediction, target, mask, robot_weight):
     mask = mask.type(torch.bool)
     repeat_mask = mask.repeat(1,3,1,1) # repeat channel dim
     diff[repeat_mask] *= robot_weight
-    num_world_pixels = (~repeat_mask).sum()
-    mean_squared_err = torch.sum(diff.abs_()) / num_world_pixels
-    return mean_squared_err
+    num_world_pixels = (~repeat_mask).sum((1,2,3))
+    mean_err = torch.mean((diff.abs_()).sum((1,2,3)) / num_world_pixels)
+    return mean_err
 
 def robot_mse_criterion(prediction, target, mask):
     """
@@ -49,9 +49,9 @@ def robot_mse_criterion(prediction, target, mask):
     mask = mask.type(torch.bool)
     repeat_mask = mask.repeat(1,3,1,1) # repeat channel dim
     diff[~repeat_mask] = 0
-    num_robot_pixels = repeat_mask.sum()
-    mean_squared_err = torch.sum(diff ** 2) / num_robot_pixels
-    return mean_squared_err
+    num_robot_pixels = repeat_mask.sum((1,2,3))
+    mean_err = torch.mean((diff ** 2).sum((1,2,3)) / num_robot_pixels)
+    return mean_err
 
 def world_mse_criterion(prediction, target, mask):
     """
@@ -63,9 +63,9 @@ def world_mse_criterion(prediction, target, mask):
     mask = mask.type(torch.bool)
     repeat_mask = mask.repeat(1,3,1,1) # repeat channel dim
     diff[repeat_mask] = 0
-    num_world_pixels = (~repeat_mask).sum()
-    mean_squared_err = torch.sum(diff ** 2) / num_world_pixels
-    return mean_squared_err
+    num_world_pixels = (~repeat_mask).sum((1,2,3))
+    mean_err = torch.mean((diff ** 2).sum((1,2,3)) / num_world_pixels)
+    return mean_err
 
 
 def kl_criterion(mu1, logvar1, mu2, logvar2, bs):