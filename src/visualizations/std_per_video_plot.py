c402c0a7d3c7616fb95b59edd5895864747a21da
Edward Hu <hues@usc.edu>
2021-11-18 17:33:48 -0500

    plotting

--- src/visualizations/std_per_video_plot.py
+++ src/visualizations/std_per_video_plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
-def plot_xy():
+def plot_xy(m1_path, m2_path, metric, plot_name):
     # 1 robot to Franka
     # method1_metrics_path = "ckpt_eval/fulllocobot_roboaware_aug_franka/metrics.pkl"
     # method2_metrics_path = "ckpt_eval/fulllocobot_vanillastate_aug_franka/metrics.pkl"
@@ -12,20 +12,15 @@ def plot_xy():
     # method2_metrics_path = "ckpt_eval/0shotlb_vanillastate_franka/metrics.pkl"
 
     # multirobot? to modified
-    method1_metrics_path = "ckpt_eval/0shotlb_roboaware_modified/metrics.pkl"
-    method2_metrics_path = "ckpt_eval/0shotlb_vanillastate_modified/metrics.pkl"
-    with open(method1_metrics_path, "rb") as f:
+    # method1_metrics_path = "ckpt_eval/0shotlb_roboaware_modified/metrics.pkl"
+    # method2_metrics_path = "ckpt_eval/0shotlb_vanillastate_modified/metrics.pkl"
+    with open(m1_path, "rb") as f:
         m1_metrics = pickle.load(f)
-    with open(method2_metrics_path, "rb") as f:
+    with open(m2_path, "rb") as f:
         m2_metrics = pickle.load(f)
 
-    METRIC = "ssim"
-    # plot_name = f"franka_0shot_multirobot_{METRIC}.pdf"
-    # plot_name = f"franka_0shot_1robot_{METRIC}.pdf"
-    plot_name = f"modified_0shot_multirobot_{METRIC}.pdf"
-
-    m1 = m1_metrics[METRIC]
-    m2 = m2_metrics[METRIC]
+    m1 = m1_metrics[metric]
+    m2 = m2_metrics[metric]
     fig, ax = plt.subplots()
     plt.scatter(x=m1, y=m2, marker=".")
     # draw diag line
@@ -40,10 +35,48 @@ def plot_xy():
     ax.set_xlim(lims)
     ax.set_ylim(lims)
 
-    plt.xlabel(f"RA {METRIC.upper()}")
-    plt.ylabel(f"VF+State {METRIC.upper()}")
-    plt.title(f"{METRIC.upper()} Error per Video of VFS and RA")
+    plt.xlabel(f"RA {metric.upper()}")
+    plt.ylabel(f"VF+State {metric.upper()}")
+    plt.title(f"{metric.upper()} Error per Video of VFS and RA")
+    plt.savefig(plot_name + ".pdf")
+    plt.savefig(plot_name + ".png", dpi=400)
     plt.savefig(plot_name)
 
+    # count how many times m1 is better than m2 for the metric
+    m1 = np.asarray(m1)
+    m2 = np.asarray(m2)
+    m1_better = np.sum(m1 > m2)
+    total = len(m1)
+    print(metric, "m1 is better", f"{m1_better} / {total}")
+
 if __name__ == "__main__":
-    plot_xy()
+    print("1 robot to Franka")
+    m1_path = "ckpt_eval/fulllocobot_roboaware_aug_franka/metrics.pkl"
+    m2_path = "ckpt_eval/fulllocobot_vanillastate_aug_franka/metrics.pkl"
+    metric="psnr"
+    plot_name = f"franka_0shot_1robot_{metric}"
+    plot_xy(m1_path, m2_path, metric, plot_name)
+    metric="ssim"
+    plot_name = f"franka_0shot_1robot_{metric}"
+    plot_xy(m1_path, m2_path, metric, plot_name)
+
+
+    print("multirobot to Franka")
+    m1_path = "ckpt_eval/0shotlb_roboaware_franka/metrics.pkl"
+    m2_path = "ckpt_eval/0shotlb_vanillastate_franka/metrics.pkl"
+    metric="psnr"
+    plot_name = f"franka_0shot_multirobot_{metric}"
+    plot_xy(m1_path, m2_path, metric, plot_name)
+    metric="ssim"
+    plot_name = f"franka_0shot_multirobot_{metric}"
+    plot_xy(m1_path, m2_path, metric, plot_name)
+
+    print("multirobot to Modified WidowX")
+    m1_path = "ckpt_eval/0shotlb_roboaware_modified/metrics.pkl"
+    m2_path = "ckpt_eval/0shotlb_vanillastate_modified/metrics.pkl"
+    metric="psnr"
+    plot_name = f"modified_0shot_multirobot_{metric}"
+    plot_xy(m1_path, m2_path, metric, plot_name)
+    metric="ssim"
+    plot_name = f"modified_0shot_multirobot_{metric}"
+    plot_xy(m1_path, m2_path, metric, plot_name)
